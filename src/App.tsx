dc1d60b15d2cd8855f280fa2d56390c33c491a24
Nilshan Deemantha <ndeemantha1999@gmail.com>
2025-05-25 02:01:09 +0530

    Fix: Remove unused logo import and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
+    <Router basename="/sattwa-beauty">
       <Navbar />
       <main className="main-content">
         {/* Your page content will go here */}
         <h1>Welcome to SATTWA Ayurvedic Beauty Care Center</h1>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
